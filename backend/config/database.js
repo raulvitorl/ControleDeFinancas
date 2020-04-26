d7c1f0d29b977f14c9b03009c0261c24a75e2589
Raul Vitor L <raulawliet@gmail.com>
2020-04-26 17:28:33 -0500

    Adicionando contador de circulos de pagamento

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,2 +1,3 @@
 const mongoose = require('mongoose')
 module.exports = mongoose.connect('mongodb://localhost/db_finance',{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório"
